667d64b2905bb40480c424b09bc3bfeba57963e7
Samarth Sanjay Pyati <samarthpyati@gmail.com>
2024-02-27 09:57:30 +0530

    Added text recognising.

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import cv2
 import numpy as np
 import screeninfo
+import pytesseract
+from numba import njit
 
 VID_HEIGHT = 1080
 VID_WIDTH = 1920
-DEFAULT_FPS = 10
-
+DEFAULT_FPS = 60
+# SETTING FUNCTIONS
 SET_AUTO_RESOLUTION = True
+RECOGNISE_TEXT = False
+
+
+@njit
+def recognise_text(_image):
+    ''' Capture the text from image '''
+    return pytesseract.image_to_string(image=_image)
+
 
 WINDOW_NAME = "WEBCAM"
 
@@ -20,14 +30,10 @@ else:
     cap.set(3, VID_WIDTH)                                   # WIDTH
     cap.set(4, VID_HEIGHT)                                  # HEIGHT
 
-
-
 cap.set(cv2.CAP_PROP_FPS, DEFAULT_FPS)                      # FPS
 cap.set(cv2.CAP_PROP_BRIGHTNESS, 150)                       # BRIGHTNESS
 
-
 print(cap.get(cv2.CAP_PROP_FPS))
-
 
 # MAIN LOOP
 while cap.isOpened():
@@ -35,16 +41,19 @@ while cap.isOpened():
         success, img = cap.read()
         __img_cpy = img.copy()
 
-        # __hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-        # __gray_scaled = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        __hsv = cv2.cvtColor(img, cv2.COLOR_BGR2LUV)
+        __gray_scaled = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-        cv2.imshow(WINDOW_NAME, img)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        cv2.imshow(WINDOW_NAME, __gray_scaled)
+        if RECOGNISE_TEXT:
+            print(pytesseract.image_to_string(image=__img_cpy))
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):               # 'q' to quit
             break
+
     except KeyboardInterrupt:
-        print("QUITTING: KEY_INTERRUPTION .. ")
+        print("QUITTING: KEY_INTERRUPTION ...")
         break
 
 cap.release()
 cv2.destroyAllWindows()
-
